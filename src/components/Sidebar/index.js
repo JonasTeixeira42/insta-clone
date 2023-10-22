774b3074fe320df49a741e429c1fb82c5344084b
Jonas Teixeira <jonaslagoteixeira@gmail.com>
2023-10-21 23:28:07 -0300

    feat: initial commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,15 @@
+import { Logo } from '../Logo'
+
+import './styles.css'
+
+const Sidebar = () => {
+  return (
+    <aside class="sidebar">
+      <div class="logo">
+        <Logo />
+      </div>
+    </aside>
+  )
+}
+
+export { Sidebar }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import { Sidebar } from '../../components/Sidebar'
+
+import './styles.css'
+
+const Home = () => {
+  return (
+    <section class="home">
+      <Sidebar />
+      <main class="main"></main>
+    </section>
+  )
+}
+
+export { Home }
